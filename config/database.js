af00278e3c633afd8524c1c4b2599636540dc544
varshithag <varshitha.1guruswamy@gmail.com>
2019-06-29 00:50:44 +0530

    updated user authentication

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mongoose=require('mongoose')
 mongoose.Promise=global.Promise
 
 mongoose.connect('mongodb://localhost:27017/contact-app',{useNewUrlParser:true})
-.then((res)=>{
-    console.log('connected to db')
-})
-.catch((err)=>{
-    console.log('erro connecting to db')
-})
+    .then((res)=>{
+         console.log('connected to db')
+    })
+    .catch((err)=>{
+             console.log('erro connecting to db')
+    })
 module.exports=mongoose
