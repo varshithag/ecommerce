75eb1c98b0261ff025cb03f24d3479a78ff7b925
varshithag <varshitha.1guruswamy@gmail.com>
2019-06-29 15:06:18 +0530

    mudified index file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const port=3000
+const cors=require('cors')
+const {mongoose}=require('./config/database')
+const addressRouter=require('./app/controller/addressController')
+const usersRouter=require('./app/controller/userController')
+app.use(cors())
+app.use(express.json())
+app.use('/users',usersRouter)
+app.use('/address',addressRouter)
+app.get('/',function(request,response){
+    response.send('welcome to the website')
+})
+app.listen(port,()=>{
+    console.log('listening to port',port)
+})
