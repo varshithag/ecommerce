2ea03c0b4fb83d9486431f2dceac93f199643b4d
varshithag <varshitha.1guruswamy@gmail.com>
2019-06-30 15:02:41 +0530

    updated category branch

---
+++ app/controller/categoryController.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const router=express.Router()
+const Category=require('../model/category')
+const authouriesUser=require('../middlewares/adminauthentication')
+
+router.get('/',(req,res)=>{
+        Category.find()
+        .then(categories=> res.json(categories))
+        .catch(err=> res.json(err))
+})
+
+router.post('/',(req,res)=>{
+    const body=req.body
+    const category=new Category(body)
+    category.save()
+    .then(val=>{
+        res.send(val)
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+
+})
+router.delete('/:id',(req,res)=>{
+    const id=req.params.id
+    Category.findByIdAndDelete(id)
+    .then(category=>{
+        console.log(category)
+        res.send(category)
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+})
+const categoryRouter=router
+module.exports=categoryRouter
--- index.js
+++ index.js
@@ -8,10 +8,12 @@ app.use(express.json())
 const mongoose=require('./config/database')
 const addressRouter=require('./app/controller/addressController')
 const usersRouter=require('./app/controller/userController')
+const categoryRouter=require('./app/controller/categoryController')
 // 
 
 app.use('/users',usersRouter)
 app.use('/address',addressRouter)
+app.use('/category',categoryRouter)
 
 app.listen(port,function(){
     console.log('listening to port',port)
